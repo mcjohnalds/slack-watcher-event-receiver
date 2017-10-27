6e2c1462b8144714322d70fd42d9e1ef97b6d017
John Owen <john.owen@aginicx.com>
2017-10-27 19:55:38 +1000

    Publish to topic on slack event

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-exports.handshake = (req, res) => {
-  res.send(req.body.message);
-}
+require("dotenv").config();
+const pubsub = require("@google-cloud/pubsub")({
+  projectId: process.env.GOOGLE_PROJECT_ID,
+  keyFilename: process.env.KEY_FILE
+});
+const topic = pubsub.topic(process.env.PUBSUB_TOPIC);
+const publisher = topic.publisher();
+
+exports.event = (req, res) => {
+  if (!isRequestValid(req)) {
+    res.end();
+    return;
+  }
+  if (req.body.type === "url_verification") {
+    res.send(req.body.challenge);
+    return;
+  }
+  // Slack docs recommends responding ASAP and processing later
+  res.end();
+  if (req.body.type === "event_callback") {
+    const event = req.body.event;
+    publishEvent(event);
+  }
+};
+
+const isRequestValid = req =>
+  req.method === "POST" &&
+  req.body.token === process.env.SLACK_VERIFICATION_TOKEN;
+
+const publishEvent = event =>
+    publisher
+      .publish(new Buffer(JSON.stringify(event)))
+      .catch(err => console.error(err));
